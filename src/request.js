427910f4424f721a4c4d1e8fc285e36d66bfd572
willerhiago <willer.hiago@gmail.com>
2019-09-30 00:38:16 -0300

    Some changes

--- src/request.js
+++ src/request.js
@@ -1,50 +1,64 @@
 const axios = require('axios');
 const shell = require("shelljs");
 
+
 var data = [];
 var json = {};
 var cursor = null;
+var self = this;
+var count = 0;
 
 module.exports = {
-    
-    getContribuidores(row){
-        return axios.get(`https://api.github.com/repos/${row.node.nameWithOwner}/contributors`).then( response=>{
-            row.node.qtdContr = response.data.length; 
-            return row; 
+
+    getReportMetrics(repo){
+        return axios.get(`http://localhost:9000/api/measures/component?componentKey=${repo.node.nameWithOwner}&metricKeys=ncloc,complexity,bugs,vulnerabilities,code_smells`).then(response=>{
+            shell.ShellString(JSON.stringify(response.data)+',').toEnd('reports.json');
         });
+    },
+
+    getContribuidores(row){
+        try{
+            shell.echo(++count);
+            return axios.get(`https://api.github.com/repos/${row.node.nameWithOwner}/contributors`).then( response=>{
+                row.node.qtdContr = response.data.length; 
+            return row; 
+            });
+        }catch(e){
+            shell.echo(e);
+            
+        }
+        
     },
     
     concatResults(response){
         var json = response;
         var array = module.exports.json2array(json['data']['search']['edges']);
-        cursor = response.data.search.pageInfo.endCursor;
+        self.cursor = response.data.search.pageInfo.endCursor;
         // let array = module.exports.json2array(json['data']['search']['edges']);
         data = data.concat(array);
     },
     
     requestData(cursor){
-        // try{
-
-        // }catch{
-
-        // }
         var url = 'https://api.github.com/graphql';
         var after = '';
         if(cursor) after = `, after:"${cursor}"`;
         var params = JSON.stringify({
-                        query : `query example { search(query: "language:python", first: 20, type: REPOSITORY ${after}) { pageInfo { hasNextPage endCursor } edges { node { ... on Repository{ name nameWithOwner createdAt updatedAt } } } } }`
+                        query : `query example { search(query: "language:kotlin", first: 20, type: REPOSITORY ${after}) { pageInfo { hasNextPage endCursor } edges { node { ... on Repository { name nameWithOwner createdAt updatedAt stargazers{ totalCount } } } } } }`
                      });
         
-        axios.defaults.headers.common['Authorization'] = 'Bearer 988d9fe5deff42249965a2b4e2bf8306357bba00';
+        axios.defaults.headers.common['Authorization'] = 'Bearer e0ce543a422019073e0d5724de522258f37149f5';
         return axios.post(url, params).then( response =>{
            module.exports.concatResults(response.data);
-            if(data.length <= 1000) module.exports.requestData(cursor);
+           shell.echo(data.length); 
+            if(data.length <= 1000) module.exports.requestData(self.cursor);
             if(data.length == 1000){
+                shell.ShellString(JSON.stringify(data)).to("data.json");
                 var promises = [];
                 data.forEach( row =>{
                     promises.push(module.exports.getContribuidores(row));    
                 });
                 return Promise.all(promises).then( data =>{
+                    shell.echo(data);
                     shell.ShellString(JSON.stringify(data)).to("data.json");
                 });
             }
@@ -63,4 +77,5 @@ module.exports = {
 
 }
 
-module.exports.requestData();
+module.exports.requestData(self.cursor);
+// module.exports = getReportMetrics;
--- src/script.js
+++ src/script.js
@@ -1,41 +1,44 @@
 const shell = require("shelljs");
-const axios = require("axios");
-const Promise = require('promise');
+const request = require("./request");
 
+
+/**
+ * Necessário rodar o script "npm run requestData" para requisição dos dados a serem analisados 
+ */
 var config;
 var data = JSON.parse(shell.cat('data.json'));
 var array = Object.keys(data).map(i => data[Number(i)]);
 
-array.forEach(function(repo){
-    if (!shell.test('-e',repo.node.name) && shell.exec(`git clone https://github.com/${repo.node.nameWithOwner}.git`).code !== 0) {
+var countRepos = 0;
+
+for (let index = 0; index < array.length; index++) {
+    if (!shell.test('-e',array[index].node.name) && shell.exec(`git clone https://github.com/${array[index].node.nameWithOwner}.git`).code !== 0) {
         shell.echo('Error: Git cloning falhou!');
-        shell.exit(1);
     }else{
-        config = `sonar.host.url=http://localhost:9000
-                  sonar.projectKey=${repo.node.nameWithOwner}
-                  sonar.projectName=${repo.node.nameWithOwner}
+        try{
+            config = `sonar.host.url=http://localhost:9000
+                  sonar.projectKey=${array[index].node.nameWithOwner}
+                  sonar.projectName=${array[index].node.nameWithOwner}
                   sonar.projectVersion=1.0
-                  sonar.projectBaseDir=${repo.node.name}
+                  sonar.projectBaseDir=${array[index].node.name}
+                  sonar.sourceEncoding=UTF-8
                   sonar.sources=.
                   sonar.java.binaries=.
                   sonar.exclusions=**/*.ts
-                  sonar.report.export.path=${repo.node.name}report.json`;
+                  sonar.report.export.path=${array[index].node.name}report.json`;
         
-        shell.ShellString(config).to('sonar-scanner/conf/sonar-scanner.properties');
-        shell.exec('sonar-scanner ');
-        shell.rm('-rf', repo.node.name);
-        getReportMetrics(repo);
-        // new Promise((resolve,reject)=>{
-        //     getReportMetrics(repo).then(callback=>{
-        //         resolve(true);
-        //     });
-        // });
+            shell.ShellString(config).to('sonar-scanner/conf/sonar-scanner.properties');
+            shell.exec('sonar-scanner ');
+            //shell.rm('-rf', array[index].node.name);
+            countRepos++;
+            shell.echo('---------------------------------');
+            shell.echo('Repositorios analisados: '+ countRepos);
+            shell.echo('---------------------------------');
+            request.getReportMetrics(array[index]);
+        }catch(e){
+            shell.echo('Erro: '+ array[index].node.nameWithOwner );
+            shell.echo(e);
+        }
     }
-});
-
-function getReportMetrics(repo){
-    return axios.get(`http://localhost:9000/api/measures/component?componentKey=${repo.node.nameWithOwner}&metricKeys=ncloc,complexity,bugs,vulnerabilities,code_smells`).then(response=>{
-        shell.ShellString(JSON.stringify(response.data)+',').toEnd('reports.json');
-        shell.echo('oi');
-    });
+    
 }
